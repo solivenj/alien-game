49c4dea3ebdb16560f5f7ea28c37fdf12d5cf74d
solivenj <soliven.j@husky.neu.edu>
2019-06-07 14:37:45 -0400

    Added ship movement functionality

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,8 +4,11 @@ import pygame
 
 from settings import Settings
 from ship import Ship
+import game_functions as gf
 
 def run_game():
+    """Method to run entire game"""
+
     # Initialize game and create a screen object.
     pygame.init()
     ai_settings = Settings()
@@ -14,21 +17,12 @@ def run_game():
     pygame.display.set_caption("Alien Invasion")
 
     # Make a ship
-    ship = Ship(screen)
+    ship = Ship(ai_settings, screen)
 
     # Start the main loop for the game
     while True:
-
-        # Watch for keyboard and mouse events.
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-
-        # Redraw the screen during each pass through the loop.
-        screen.fill(ai_settings.bg_color)
-        ship.blitme()
-        
-        # Make the most recently drawn screen visible.
-        pygame.display.flip()
+        gf.check_events(ship)
+        ship.update()
+        gf.update_screen(ai_settings, screen, ship)
 
 run_game()
